3a02e9856758263bcce057736c5c780ac35e9633
zhangtao <tzhang920314@gmail.com>
2015-12-29 10:36:18 +0800

    add user api

---
+++ api/controllers/UserController.js
@@ -0,0 +1,51 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `UserController.create()`
+   */
+  create: function (req, res) {
+    return res.json({
+      todo: 'create() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `UserController.update()`
+   */
+  update: function (req, res) {
+    return res.json({
+      todo: 'update() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `UserController.delete()`
+   */
+  delete: function (req, res) {
+    return res.json({
+      todo: 'delete() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `UserController.find()`
+   */
+  find: function (req, res) {
+    return res.json({
+      todo: 'find() is not implemented yet!'
+    });
+  }
+};
+
---
+++ api/models/User.js
@@ -0,0 +1,21 @@
+/**
+* User.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+
+    create : { type: 'string' },
+
+    update : { type: 'string' },
+
+    delete : { type: 'string' },
+
+    find : { type: 'string' }
+  }
+};
+
