291b57c0284cb954d9b73fc9ce243f72cc81bb5c
zhangtao <tzhang920314@gmail.com>
2015-12-30 17:05:46 +0800

    add api post

---
+++ api/controllers/PostController.js
@@ -0,0 +1,50 @@
+/**
+ * PostController
+ *
+ * @description :: Server-side logic for managing posts
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `PostController.create()`
+   */
+  create: function (req, res) {
+    return res.json({
+      todo: 'create() is not implemented yet!'
+    })  },
+
+
+  /**
+   * `PostController.delete()`
+   */
+  delete: function (req, res) {
+    return res.json({
+      todo: 'delete() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `PostController.find()`
+   */
+  find: function (req, res) {
+    return res.json({
+      todo: 'find() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `PostController.update()`
+   */
+  update: function (req, res) {
+    return res.json({
+      todo: 'update() is not implemented yet!'
+    });
+  }
+};
+
---
+++ api/models/Post.js
@@ -0,0 +1,21 @@
+/**
+* Post.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+
+    create : { type: 'string' },
+
+    delete : { type: 'string' },
+
+    find : { type: 'string' },
+
+    update : { type: 'string' }
+  }
+};
+
